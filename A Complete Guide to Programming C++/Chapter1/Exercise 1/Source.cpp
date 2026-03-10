a105875df6545c0fc27965c2aab08148f959a5d5
CodeCrocodileDroid <yaomao694@gmail.com>
2026-03-10 15:53:30 +0200

    Add files via upload

---
+++ A Complete Guide to Programming C++/Chapter1/Exercise 1/Source.cpp
@@ -0,0 +1,17 @@
+
+/******************************************************
+Exercise 1: Display a happy day message
+******************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Oh what" << endl;
+    cout << "a happy day!" << endl;
+    cout << "Oh yes," << endl;
+    cout << "what a happy day!" << endl;
+
+    return 0;
+}
+
---
+++ A Complete Guide to Programming C++/Chapter1/Exercise 3/Source.cpp
@@ -0,0 +1,16 @@
+/******************************************************
+Exercise 3: Output text starting on a new line
+******************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << endl;  // Start with a blank line
+    cout << "Dear reader," << endl;
+    cout << "have a BREAK!" << endl;
+
+    return 0;
+}
+
+
